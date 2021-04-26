bc0fbe3e57c61a7dd606cfd3b744646296dfd66e
JuriKnauth <knauth.juri@gmail.com>
2021-04-26 22:32:39 +0200

    rename

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -1,17 +1,18 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Background : MonoBehaviour
 {
     [SerializeField] private Image _backgroundImage;
-    [SerializeField] private TextMeshProUGUI _backgroundColorText;
+    [FormerlySerializedAs("_backgroundColorText")] [SerializeField] private TextMeshProUGUI _backgroundColorTextRenamed;
     [SerializeField] private Color _backgroundColorRenamed;
 
     private void Awake()
     {
         _backgroundImage.color = _backgroundColorRenamed;
 
-        _backgroundColorText.text = _backgroundColorRenamed.ToString();
+        _backgroundColorTextRenamed.text = _backgroundColorRenamed.ToString();
     }
 }
